c5643664b2e23d969f41eda10a42bdc27d91223d
valya <valia2006@DESKTOP-KJRGD7H.localdomain>
2025-11-02 11:00:56 +0300

    second step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,6 +9,9 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version(packageJson.version);
+  .version(packageJson.version)
+  .option('-f --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
+
 
 program.parse(process.argv);
